d0ab5a51d3b706da5e4657e4f9248ed3bb2605e3
[디렉션소프트]박찬웅 <chanwoong.park01@lotte.net>
2020-09-11 13:55:03 +0900

    프로그래머스) 가운데 글자 가져오기 - 해결 및 다른 사람의 풀이

--- Programmers/Level1/Lessons12903.playground/Contents.swift
+++ Programmers/Level1/Lessons12903.playground/Contents.swift
@@ -1,5 +1,37 @@
 import UIKit
 
 func solution(_ s:String) -> String {
-    return ""
+    var arr: [String] = s.map{"\($0)"}
+    var targetIndex = 0
+    var answer = ""
+
+    if arr.count % 2 == 0 { // 짝수
+        if arr.count == 2 {
+            answer = arr.reduce("", +)
+        } else {
+            for _ in arr where arr.count > 2 {
+                arr.removeLast()
+                arr.removeFirst()
+            }
+            answer = arr.reduce("", +)
+        }
+    } else { //홀수
+       targetIndex = arr.count / 2
+        answer = arr[targetIndex]
+    }
+
+    return answer
 }
+
+solution("qwer")
+
+// 다른 사람의 풀이
+
+//func solution(_ s:String) -> String {
+//
+//    if Array(s).count % 2 == 0 {
+//        return String(Array(s)[(s.count/2)-1...(s.count/2)])
+//    }else{
+//        return String(Array(s)[s.count/2])
+//    }
+//}
