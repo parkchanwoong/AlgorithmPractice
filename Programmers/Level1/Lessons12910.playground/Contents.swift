0e97a56fce3276c49eec53b98bfc9bab7829b714
[디렉션소프트]박찬웅 <chanwoong.park01@lotte.net>
2020-09-11 14:21:27 +0900

    프로그래머스) 나누어 떨어지는 숫자 배열 - 해결 및 다른 사람의 풀이

--- Programmers/Level1/Lessons12910.playground/Contents.swift
+++ Programmers/Level1/Lessons12910.playground/Contents.swift
@@ -1,5 +1,15 @@
 import UIKit
 
 func solution(_ arr:[Int], _ divisor:Int) -> [Int] {
-    return []
+    let arrTemp = arr.sorted().filter {$0 % divisor == 0}
+    return arrTemp.isEmpty ? [-1] : arrTemp
 }
+
+solution([3,2,6], 10)
+
+// 다른 사람의 풀이
+
+//func solution(_ arr:[Int], _ divisor:Int) -> [Int] {
+//    let array = arr.sorted().filter{ $0 % divisor == 0 }
+//    return  array == [] ? [-1] : array
+//}
