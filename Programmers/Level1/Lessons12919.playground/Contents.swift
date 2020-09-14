8f4959d6863a0160824f556d4114c11ff6b237b5
[디렉션소프트]박찬웅 <chanwoong.park01@lotte.net>
2020-09-14 17:59:14 +0900

    프로그래머스) 서울에서 김서방 찾기 - 해결 및 다른 사람의 풀이

--- Programmers/Level1/Lessons12919.playground/Contents.swift
+++ Programmers/Level1/Lessons12919.playground/Contents.swift
@@ -1,5 +1,11 @@
 import UIKit
 
 func solution(_ seoul:[String]) -> String {
-    return ""
+    return "김서방은 \(seoul.firstIndex(of: "Kim")!)에 있다"
 }
+
+solution(["Jane","Kim"])
+
+// 다른 사람의 풀이
+
+// 나랑 같음 ㅋ
