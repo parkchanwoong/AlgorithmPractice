ae81de71503075c65716644aef688bf27813b7e3
[디렉션소프트]박찬웅 <chanwoong.park01@lotte.net>
2020-09-14 13:21:32 +0900

    프로그래머스) 문자열 내 마음대로 정렬하기 - 해결 및 다른 사람의 풀이

--- Programmers/Level1/Lessons12915.playground/Contents.swift
+++ Programmers/Level1/Lessons12915.playground/Contents.swift
@@ -1,5 +1,22 @@
 import UIKit
 
 func solution(_ strings:[String], _ n:Int) -> [String] {
-    return []
+    
+    var dicTemp = [String:String]()
+    for str in strings {
+        for (index,value) in str.enumerated() where index == n {
+            dicTemp[str] = "\(value)"
+        }
+    }
+    let dicTemp2 = dicTemp.sorted { $0.key < $1.key }.sorted{ $0.value < $1.value}
+
+    return dicTemp2.map {$0.key}
 }
+
+solution(["abce","abcd","cdx"], 2)
+
+// 다른 사람의 풀이
+
+//func solution(_ strings:[String], _ n:Int) -> [String] {
+//    return strings.sorted{  Array($0)[n] == Array($1)[n] ? $0 < $1 :  Array($0)[n] < Array($1)[n] }
+//}
