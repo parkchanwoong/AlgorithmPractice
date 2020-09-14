71d6eb25aadd288efe1959c05915b0efc5aea2d3
[디렉션소프트]박찬웅 <chanwoong.park01@lotte.net>
2020-09-14 17:17:22 +0900

    프로그래머스) 문자열 내 p와 y의 개수 - 해결 및 다른 사람의 풀이_2

--- Programmers/Level1/Lessons12916.playground/Contents.swift
+++ Programmers/Level1/Lessons12916.playground/Contents.swift
@@ -6,8 +6,15 @@ func solution(_ s:String) -> Bool
 
     let pCount = s.lowercased().filter {$0 == "p"}.count
     let yCount = s.lowercased().filter {$0 == "y"}.count
-    ans = pCount == yCount ? true : false 
+    ans = pCount == yCount ? true : false
     return ans
 }
 
 solution("pPoooyY")
+
+// 다른 사람의 풀이
+
+//func solution(_ s:String) -> Bool
+//{
+//    return string.components(separatedBy: "p").count == string.components(separatedBy: "y").count
+//}
