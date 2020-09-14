4df86482340dddfc356e48858fa9ca6a495b9180
[디렉션소프트]박찬웅 <chanwoong.park01@lotte.net>
2020-09-14 10:58:54 +0900

    프로그래머스) 두 정수 사이의 합 - 해결 및 다른 사람의 풀이

--- Programmers/Level1/Lessons12912.playground/Contents.swift
+++ Programmers/Level1/Lessons12912.playground/Contents.swift
@@ -1,5 +1,15 @@
 import UIKit
-
+// 시간초과로 통과가 안되는 경우가 있음
 func solution(_ a:Int, _ b:Int) -> Int64 {
-    return 0
+    let arr = a < b ? Array(a...b) : Array(b...a)
+    return Int64(arr.reduce(0, +))
 }
+
+solution(5, 3)
+
+// 다른 사람의 풀이
+
+
+//func solution(_ a:Int, _ b:Int) -> Int64 {
+//    return Int64(Array(a > b ? b...a : a...b).reduce(0, +))
+//}
