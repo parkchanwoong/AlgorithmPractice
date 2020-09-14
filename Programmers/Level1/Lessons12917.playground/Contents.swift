c7e5916ac29349ffc8a66a7b44e3c9d35a44c721
[디렉션소프트]박찬웅 <chanwoong.park01@lotte.net>
2020-09-14 17:31:01 +0900

    프로그래머스) 문자열 내림차순으로 배치하기 - 해결 및 다른 사람의 풀이

--- Programmers/Level1/Lessons12917.playground/Contents.swift
+++ Programmers/Level1/Lessons12917.playground/Contents.swift
@@ -1,5 +1,18 @@
 import UIKit
 
 func solution(_ s:String) -> String {
-    return ""
+    let strings = s.sorted(by: >)
+    var answer = ""
+    for str in strings {
+        answer += "\(str)"
+    }
+    return answer
 }
+
+solution("Zbcdefg")
+
+// 다른 사람의 풀이
+
+//func solution(_ s:String) -> String {
+//    return String(s.sorted { $0 > $1 })
+//}
