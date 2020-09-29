1344e0bb5c8d1ea7f1aa23efce695bb5a633dfce
[디렉션소프트]박찬웅 <chanwoong.park01@lotte.net>
2020-09-29 11:28:32 +0900

    프로그래머스) 완전탐색 (모의고사)  - 해결x, 다른 사람의 풀이

---
+++ Programmers/Level1/Lessons42840.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+func solution(_ answers:[Int]) -> [Int] {
+    let answer = (
+        a: [1, 2, 3, 4, 5], // index % 5
+        b: [2, 1, 2, 3, 2, 4, 2, 5], // index % 8
+        c: [3, 3, 1, 1, 2, 2, 4, 4, 5, 5] // index % 10
+    )
+    var point = [1:0, 2:0, 3:0]
+
+    for (i, v) in answers.enumerated() {
+        if v == answer.a[i % 5] { point[1] = point[1]! + 1 }
+        if v == answer.b[i % 8] { point[2] = point[2]! + 1 }
+        if v == answer.c[i % 10] { point[3] = point[3]! + 1 }
+    }
+
+    return point.sorted{ $0.key < $1.key }.filter{ $0.value == point.values.max() }.map{ $0.key }
+}
+
+solution([1,2,3,4,5])
